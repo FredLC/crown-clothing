0738145707cf267c2123cd223da47c93e0094500
fredlc <fredlefevre@outlook.com>
2019-08-27 21:19:53 -0400

    Integrated shop page and collection preview component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
 
 function App() {
-  const Hats = () => {
-    return (
-      <div>
-        <h1>HATS</h1>
-      </div>
-    );
-  };
-
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={Hats} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
