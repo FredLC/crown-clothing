bc1e263e8949b7c5ca12312b2b10f9852bbbda73
fredlc <fredlefevre@outlook.com>
2019-10-27 12:45:03 -0400

    Added redux store and user actions file

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddlewares } from "redux";
+import logger from "redux-logger";
+import rootReducer from "./root-reducer";
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddlewares(...middlewares));
+
+export default store;
