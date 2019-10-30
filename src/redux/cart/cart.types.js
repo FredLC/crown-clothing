db06faed4b5f007271d5ff27f22d3e7d44ee948e
fredlc <fredlefevre@outlook.com>
2019-10-30 16:37:12 -0400

    Added clear and remove item functionality to checkout item component

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
   toggleCartDropdown: "TOGGLE_CART_DROPDOWN",
-  addItem: "ADD_ITEM"
+  addItem: "ADD_ITEM",
+  removeItem: "REMOVE_ITEM",
+  clearItemFromCart: "CLEAR_ITEM_FROM_CART"
 };
 
 export default CartActionTypes;
