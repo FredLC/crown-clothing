68f445f7a3d751756a3ec6043b0ddb7a38c0036b
fredlc <fredlefevre@outlook.com>
2020-01-17 19:05:17 -0500

    Retrieved shop data from Firebase and added it to redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -53,6 +53,23 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+  return transformedCollection.reduce((acc, collection) => {
+    acc[collection.title.toLowerCase()] = collection;
+    return acc;
+  }, {});
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
