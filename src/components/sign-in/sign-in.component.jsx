7430f54ba449a6c988926c0d6a6399a3014ec632
fredlc <fredlefevre@outlook.com>
2020-05-16 13:28:59 -0400

    Refactored Google sign in into sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,74 +1,87 @@
-import React, { Component } from "react";
-import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-button/custom-button.component";
-import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
-import "./sign-in.styles.scss";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { googleSignInStart } from '../../redux/user/user.actions';
+import './sign-in.styles.scss';
 
-export default class SignIn extends Component {
-  constructor(props) {
-    super(props);
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
-
-  handleSubmit = async e => {
-    e.preventDefault();
-
-    const { email, password } = this.state;
-
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({
-        email: "",
-        password: ""
-      });
-    } catch (error) {
-      console.log(error);
+        this.state = {
+            email: '',
+            password: '',
+        };
     }
-  };
 
-  handleChange = e => {
-    const { value, name } = e.target;
+    handleSubmit = async (e) => {
+        e.preventDefault();
 
-    this.setState({ [name]: value });
-  };
+        const { email, password } = this.state;
 
-  render() {
-    return (
-      <div className="sign-in">
-        <h2 className="title">I already have an account</h2>
-        <span>Sign in with your email and password</span>
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: '',
+                password: '',
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            type="email"
-            name="email"
-            label="email"
-            value={this.state.email}
-            handleChange={this.handleChange}
-            required
-          />
+    handleChange = (e) => {
+        const { value, name } = e.target;
 
-          <FormInput
-            type="password"
-            name="password"
-            label="password"
-            value={this.state.password}
-            handleChange={this.handleChange}
-            required
-          />
+        this.setState({ [name]: value });
+    };
 
-          <div className="buttons">
-            <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign In With Google
-            </CustomButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
+    render() {
+        const { googleSignInStart } = this.props;
+        return (
+            <div className='sign-in'>
+                <h2 className='title'>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        type='email'
+                        name='email'
+                        label='email'
+                        value={this.state.email}
+                        handleChange={this.handleChange}
+                        required
+                    />
+
+                    <FormInput
+                        type='password'
+                        name='password'
+                        label='password'
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        required
+                    />
+
+                    <div className='buttons'>
+                        <CustomButton type='submit'>Sign In</CustomButton>
+                        <CustomButton
+                            type='button'
+                            onClick={googleSignInStart}
+                            isGoogleSignIn
+                        >
+                            Sign In With Google
+                        </CustomButton>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,73 +1,73 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDVSm9GE3_oQG9UrWbNJA3PU_d494BlwzQ",
-  authDomain: "crown-clothing-96720.firebaseapp.com",
-  databaseURL: "https://crown-clothing-96720.firebaseio.com",
-  projectId: "crown-clothing-96720",
-  storageBucket: "crown-clothing-96720.appspot.com",
-  messagingSenderId: "9812839855",
-  appId: "1:9812839855:web:ee987aa5b3ef79bd01f35a"
+    apiKey: 'AIzaSyDVSm9GE3_oQG9UrWbNJA3PU_d494BlwzQ',
+    authDomain: 'crown-clothing-96720.firebaseapp.com',
+    databaseURL: 'https://crown-clothing-96720.firebaseio.com',
+    projectId: 'crown-clothing-96720',
+    storageBucket: 'crown-clothing-96720.appspot.com',
+    messagingSenderId: '9812839855',
+    appId: '1:9812839855:web:ee987aa5b3ef79bd01f35a',
 };
 
 export const createUserDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+    if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapshot = await userRef.get();
+    const snapshot = await userRef.get();
 
-  if (!snapshot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
 
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData
-      });
-    } catch (error) {
-      console.log("error creating user", error.message);
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData,
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
     }
-  }
 
-  return userRef;
+    return userRef;
 };
 
 export const addCollectionAndDocuments = async (
-  collectionKey,
-  objectsToAdd
+    collectionKey,
+    objectsToAdd
 ) => {
-  const collectionRef = firestore.collection(collectionKey);
+    const collectionRef = firestore.collection(collectionKey);
 
-  const batch = firestore.batch();
-  objectsToAdd.forEach(obj => {
-    const newDocRef = collectionRef.doc();
-    batch.set(newDocRef, obj);
-  });
+    const batch = firestore.batch();
+    objectsToAdd.forEach((obj) => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
 
-  return await batch.commit();
+    return await batch.commit();
 };
 
-export const convertCollectionsSnapshotToMap = collections => {
-  const transformedCollection = collections.docs.map(doc => {
-    const { title, items } = doc.data();
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map((doc) => {
+        const { title, items } = doc.data();
 
-    return {
-      routeName: encodeURI(title.toLowerCase()),
-      id: doc.id,
-      title,
-      items
-    };
-  });
-  return transformedCollection.reduce((acc, collection) => {
-    acc[collection.title.toLowerCase()] = collection;
-    return acc;
-  }, {});
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items,
+        };
+    });
+    return transformedCollection.reduce((acc, collection) => {
+        acc[collection.title.toLowerCase()] = collection;
+        return acc;
+    }, {});
 };
 
 firebase.initializeApp(config);
@@ -75,8 +75,8 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
